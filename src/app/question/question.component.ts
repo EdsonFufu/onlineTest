5b410d78539ddde8027199dcea9e93527191d7bd
edson.fufu.21 <edson.fufu.21@gmail.com>
2022-04-28 00:36:10 +0300

    Add Count Down Timer

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { QuestionService } from '../question.service';
+import { timer, Subscription } from "rxjs";
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit,OnDestroy {
   public name:string = ""
   public email:string = ""
 
@@ -17,14 +19,41 @@ export class QuestionComponent implements OnInit {
   public questions: any = [];
   public quizCompleted = false;
 
+  countDown: Subscription = new Subscription;
+
   constructor(private questionService:QuestionService) { }
 
+
   ngOnInit(): void {
+    this.countDown = timer(0, 1000).subscribe(() => --this.counter);
     this.name = JSON.parse(localStorage.getItem("user")!).name
     this.email = JSON.parse(localStorage.getItem("user")!).email
     this.questions = this.questionService.getQuestions().subscribe(qs => {
       this.questions = qs.questions
     })
+  }
+  answer(currentQuestion:number, option: any){
+    console.log(currentQuestion, option);
+
+    if(currentQuestion == this.questions.length){
+      this.quizCompleted = true;
+    }
+
+    if(option.correct){
+      console.log("correct answer");
+      this.points += 10;
+      this.correctAnswers++;
+    } else {
+      console.log('wrong answer');
+    }
+
+    setTimeout(() => {
+      this.currentQuestion++;
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
 
 }
